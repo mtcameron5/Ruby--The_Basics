aae640affad7ec3ab2b7ddaf3b75a136520c298e
mtcameron5 <mtcameron5@gmail.com>
2020-10-09 16:43:17 -0600

    Exercise 16 updated

--- more_exercises/exercise_16.rb
+++ more_exercises/exercise_16.rb
@@ -16,3 +16,18 @@ contacts.each do |key, value|
 end
 
 puts contacts
+
+# Alternatively, without calling flatten! on the array
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"], 
+                ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = { "Joe Smith" => {}, "Sally Johnson" => {} }
+contact_fields = [:email, :address, :phone]                
+
+contacts.each_with_index do |(key, value), index| 
+  contact_fields.each do |field| 
+    contacts[key][field] = contact_data[index].shift 
+  end 
+end 
+
+p contacts
